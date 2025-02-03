a1e0415402e572a2aff4100c7af8d2b59d0107a9
Majid Mirshekari <MajidMirshekari6@gmail.com>
2025-02-03 18:42:09 +0330

    New

--- Seski.py
+++ Seski.py
@@ -1,30 +1,27 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 # تابع شروع
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('سلام! به ربات من خوش اومدی.')
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text('سلام! به ربات من خوش اومدی.')
 
 # تابع پاسخ به پیام‌ها
-def echo(update: Update, context: CallbackContext):
+async def echo(update: Update, context: CallbackContext):
     user_message = update.message.text
-    update.message.reply_text(f'شما گفتید: {user_message}')
+    await update.message.reply_text(f'شما گفتید: {user_message}')
 
 def main():
     # توکن ربات رو اینجا وارد کنید
-    updater = Updater("7569285769:AAEvKY0KhSqDVS-myc-dOs8jPj6RK2w_ghE", use_context=True)
-
-    dispatcher = updater.dispatcher
+    application = Application.builder().token("7569285769:AAEvKY0KhSqDVS-myc-dOs8jPj6RK2w_ghE").build()
 
     # دستور /start
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # پاسخ به پیام‌های کاربر
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     # شروع ربات
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
